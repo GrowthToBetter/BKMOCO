e47c79e41ccb3c387b8e8994f1e21fc00e334c4a
GrowthToBetter <baratrahjaga@gmail.com>
2024-08-05 10:24:41 +0700

    menambahkan  fitur our services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -133,24 +133,94 @@ export default function Home() {
         />
         <div className="m-10">
           <p className="text-highlight">TENTANG KAMI</p>
-          <h1 className="mt-3 font-semibold mb-2 text-xl">BK: Sahabat Siswa dan <br />Sahabat di Setiap Langkah</h1>
-          <p className="text-md">Program BK untuk memberikan dampak kepada permasalah yang terjadi <br />di dalam dan sekolah</p>
-          <div className="flex p-1 justify-start items-center"><p className="mr-1"><FiCheckCircle /> </p><p className="text-md">Layanan Siswa dan Orang Tua</p></div>
-          <div className="flex p-1 justify-start items-center"><p className="mr-1"><FiCheckCircle /></p><p className="text-md"> Kunjungan ke Rumah dan Undangan ke Sekolah</p></div>
+          <h1 className="mt-3 font-semibold mb-2 text-xl">
+            BK: Sahabat Siswa dan <br />
+            Sahabat di Setiap Langkah
+          </h1>
+          <p className="text-md">
+            Program BK untuk memberikan dampak kepada permasalah yang terjadi{" "}
+            <br />
+            di dalam dan sekolah
+          </p>
+          <div className="flex p-1 justify-start items-center">
+            <p className="mr-1">
+              <FiCheckCircle />{" "}
+            </p>
+            <p className="text-md">Layanan Siswa dan Orang Tua</p>
+          </div>
+          <div className="flex p-1 justify-start items-center">
+            <p className="mr-1">
+              <FiCheckCircle />
+            </p>
+            <p className="text-md">
+              {" "}
+              Kunjungan ke Rumah dan Undangan ke Sekolah
+            </p>
+          </div>
         </div>
-      <div className="w-1/2 h-38 top-80 left-1/3 bg-moklet absolute flex justify-center rounded-lg">
-      <div className="flex flex-col w-1/2  p-4 justify-start">
-        <h1 className="font-semibold text-xl m-5 mb-3 text-white">Misi</h1>
-        <p className="text-white ml-5 text-sm font-light">Memberikan informasi dan edukasi <br />kepada siswa tentang pentingnya <br />menjaga kesehatan mental</p>
-      </div>
-        <div className="h-full transform scale-y-75 border-r-2 border-white"></div>
-      <div className="flex flex-col p-4 w-1/2 justify-start">
-        <h1 className="font-semibold text-xl m-5 mb-3 text-white">Pendekatan</h1>
-        <p className="text-white text-sm font-light ml-5">Undang ahli kesehatan mental atau <br />psikolog untuk menulis artikel. Pastikan <br />artikel tersebut mudah dipahami oleh <br />siswa dan relevan dengan kehidupan <br />sehari-hari mereka</p>
-      </div>
-      </div>
+        <div className="w-1/2 h-38 top-80 left-1/3 bg-moklet absolute flex justify-center rounded-lg">
+          <div className="flex flex-col w-1/2  p-4 justify-start">
+            <h1 className="font-semibold text-xl m-5 mb-3 text-white">Misi</h1>
+            <p className="text-white ml-5 text-sm font-light">
+              Memberikan informasi dan edukasi <br />
+              kepada siswa tentang pentingnya <br />
+              menjaga kesehatan mental
+            </p>
+          </div>
+          <div className="h-full transform scale-y-75 border-r-2 border-white"></div>
+          <div className="flex flex-col p-4 w-1/2 justify-start">
+            <h1 className="font-semibold text-xl m-5 mb-3 text-white">
+              Pendekatan
+            </h1>
+            <p className="text-white text-sm font-light ml-5">
+              Undang ahli kesehatan mental atau <br />
+              psikolog untuk menulis artikel. Pastikan <br />
+              artikel tersebut mudah dipahami oleh <br />
+              siswa dan relevan dengan kehidupan <br />
+              sehari-hari mereka
+            </p>
+          </div>
+        </div>
       </div>
 
+      <div className="w-full h-96 flex justify-center flex-wrap p-5">
+        <div className="w-3/12 rounded-lg h-2/3 bg-moklet m-3 p-4 pl-7">
+        <p className="text-white text-md mb-3 font-light">Our Services</p>
+        <h1 className="text-white font-bold text-xl mb-10">Get treatment for all your <br />mental healthcare needs.</h1>
+        <button className="w-44 flex p-3 justify-between items-center text-white border-2 border-white rounded-lg text-md font-light">See All Service <p className="size-5 items-center flex "><FiArrowRight /></p></button>
+        </div>
+        <div className="w-3/12 flex items-center justify-center relative rounded-lg h-2/3 bg-[url('/img/4.png')] bg-no-repeat bg-cover m-3">
+        <div className="shadow-2xl flex pl-2 justify-start bg-white w-72 h-14 items-center absolute -bottom-3 rounded-lg ">
+          <p className="text-md  font-medium text-nowrap">Bullying Counseling</p>
+          <div className="rounded-full w-7 border-2 border-white -right-2 h-7 absolute flex justify-center items-center bg-moklet "><p className="text-lg text-white"><FiArrowRight /></p></div>
+        </div>
+        </div>
+        <div className="w-3/12 flex items-center justify-center relative rounded-lg h-2/3 bg-[url('/img/5.png')] bg-no-repeat bg-cover m-3">
+        <div className="shadow-2xl flex pl-2 justify-start bg-white w-72 h-14 items-center absolute -bottom-3 rounded-lg ">
+          <p className="text-md  font-medium text-nowrap">Mental Health Counseling</p>
+          <div className="rounded-full w-7 border-2 border-white -right-2 h-7 absolute flex justify-center items-center bg-moklet "><p className="text-lg text-white"><FiArrowRight /></p></div>
+        </div>
+        </div>
+        <div className="w-3/12 flex items-center justify-center relative rounded-lg h-2/3 bg-[url('/img/6.png')] bg-no-repeat bg-cover m-3">
+        <div className="shadow-2xl flex pl-2 justify-start bg-white w-72 h-14 items-center absolute -bottom-3 rounded-lg ">
+          <p className="text-md  font-medium text-nowrap">Relationship Counseling</p>
+          <div className="rounded-full w-7 border-2 border-white -right-2 h-7 absolute flex justify-center items-center bg-moklet "><p className="text-lg text-white"><FiArrowRight /></p></div>
+        </div>
+        </div>
+        <div className="w-3/12 flex items-center justify-center relative rounded-lg h-2/3 bg-[url('/img/7.png')] bg-no-repeat bg-cover m-3">
+        <div className="shadow-2xl flex pl-2 justify-start bg-white w-72 h-14 items-center absolute -bottom-3 rounded-lg ">
+          <p className="text-md  font-medium text-nowrap">Problem Counseling</p>
+          <div className="rounded-full w-7 border-2 border-white -right-2 h-7 absolute flex justify-center items-center bg-moklet "><p className="text-lg text-white"><FiArrowRight /></p></div>
+        </div>
+        </div>
+        <div className="w-3/12 flex items-center justify-center relative rounded-lg h-2/3 bg-[url('/img/8.png')] bg-no-repeat bg-cover m-3">
+        <div className="shadow-2xl flex pl-2 justify-start bg-white w-72 h-14 items-center absolute -bottom-3 rounded-lg ">
+          <p className="text-md  font-medium text-nowrap">Lack Of Motivation Counseling</p>
+          <div className="rounded-full w-7 border-2 border-white -right-2 h-7 absolute flex justify-center items-center bg-moklet "><p className="text-lg text-white"><FiArrowRight /></p></div>
+        </div>
+        </div>
+        
+    </div>
     </div>
   );
 }
