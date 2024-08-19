b398f9cb7a14edcdbc819aaca2f90220cc9b1d6a
GrowthToBetter <baratrahjaga@gmail.com>
2024-08-19 17:33:50 +0700

    mencari sumber eror component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
-import AppHero from "@/Components/AppHero";
-
+import React from "react";
+import Home from "@/Components/Home";
+import Section from "@/Components/Section";
+import About from "@/Components/About";
+import Service from "@/Components/Service";
+import Profile from "@/Components/Profile";
+import Testimoni from "@/Components/Testimoni";
+import News from "@/Components/News";
+import Credit from "@/Components/Credit";
 export default function App() {
   return (
-    <>
-      <AppHero />
-    </>
+      <div className="relative flex flex-col justify-center overflow-hidden ">
+      <Home />
+      <Section />
+      <About />
+      <Service />
+      <Profile />
+      <Testimoni />
+      <News />
+      <Credit />
+    </div>
   );
 }
