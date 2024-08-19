089ab298117496bbb30f1149386b7f11c2ee778d
GrowthToBetter <baratrahjaga@gmail.com>
2024-08-19 17:42:09 +0700

    mencari bug prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
-import Home from "@/Components/Home";
-import Section from "@/Components/Section";
-import About from "@/Components/About";
-import Service from "@/Components/Service";
-import Profile from "@/Components/Profile";
-import Testimoni from "@/Components/Testimoni";
-import News from "@/Components/News";
-import Credit from "@/Components/Credit";
+import Home from "../../Components/Home";
+import Section from "../../Components/Section";
+import About from "../../Components/About";
+import Service from "../../Components/Service";
+import Profile from "../../Components/Profile";
+import Testimoni from "../../Components/Testimoni";
+import News from "../../Components/News";
+import Credit from "../../Components/Credit";
 export default function App() {
   return (
-      <div className="relative flex flex-col justify-center overflow-hidden ">
+    <div className="relative flex flex-col justify-center overflow-hidden ">
       <Home />
       <Section />
       <About />
