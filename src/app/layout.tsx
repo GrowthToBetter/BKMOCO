f4765eb40f98c5e52afbba5278880cfc265654bb
GrowthToBetter <baratrahjaga@gmail.com>
2024-08-19 17:45:20 +0700

    mencari bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
-import Navbar from "../../Components/Navbar";
+import Navbar from "../../public/Components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
-import Home from "../../Components/Home";
-import Section from "../../Components/Section";
-import About from "../../Components/About";
-import Service from "../../Components/Service";
-import Profile from "../../Components/Profile";
-import Testimoni from "../../Components/Testimoni";
-import News from "../../Components/News";
-import Credit from "../../Components/Credit";
+import Home from "../../public/Components/Home";
+import Section from "../../public/Components/Section";
+import About from "../../public/Components/About";
+import Service from "../../public/Components/Service";
+import Profile from "../../public/Components/Profile";
+import Testimoni from "../../public/Components/Testimoni";
+import News from "../../public/Components/News";
+import Credit from "../../public/Components/Credit";
 export default function App() {
   return (
     <div className="relative flex flex-col justify-center overflow-hidden ">
