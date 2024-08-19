9a3367b9ae2be7636c01d2a544b4ab073a0b9997
GrowthToBetter <baratrahjaga@gmail.com>
2024-08-19 17:51:00 +0700

    mencari bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
-import Navbar from "../../public/Components/Navbar";
+import Navbar from "./Components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
